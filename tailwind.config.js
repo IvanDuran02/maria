21c16a9ad7e915700c9a39b94de1cf158b395438
Ivan Duran <iamivanduran02@gmail.com>
2022-08-18 15:20:25 -0400

    Cropped hero-image and styled most of the ui for the hero section.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "maria-purple": "#5267DF",
+        "maria-red": "#FA5959",
+        "maria-blue": "#242A45",
+        "maria-grey": "#9194A2",
+        "maria-white": "#f7f7f7",
+      },
+      fontFamily: {
+        Poppins: ["Poppins, sans-serif"],
+      },
+    },
   },
+
   plugins: [],
 };
